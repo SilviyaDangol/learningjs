7b81ce18c35049bbd15b60be824a0a418a871abe
sylvia0703 <96442009+sylvia0703@users.noreply.github.com>
2025-01-20 12:51:31 +0545

    ejf

--- BoredBot/index.js
+++ BoredBot/index.js
@@ -1,16 +1,13 @@
 // document.getElementById("get-activity").addEventListener("click", function() {
     fetch("https://apis.scrimba.com/bored/api/activity")
     .then(response=> {
-      
-      response.json()
+      console.log(response.status);
+      return response.json()
     })
-      // console.log(response.status);
-      // return response.json()})
     .then(data => {
         console.log(data)
-      //   document.getElementById("activity").textContent = data.activity
-      //   document.getElementById("title").textContent = "🦾 HappyBot🦿"
-      // document.body.classList.add("fun")
+        document.getElementById("activity").textContent = data.activity
+        document.getElementById("title").textContent = "🦾 HappyBot🦿"
+        document.body.classList.add("fun")
     })
-// })
 
